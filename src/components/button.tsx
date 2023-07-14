adb67d5fb064628cf4f7c076ee4e62d4a4f8867a
Jeanette Yue <j-yue>
2023-07-13 18:35:07 -0700

    Create component

---
+++ src/components/button.tsx
@@ -0,0 +1,23 @@
+type ButtonProps = {
+  text: string;
+  // if not declared, assume secondary button
+  primary?: string;
+  children?: React.ReactNode;
+};
+
+export default function Button({ text, primary, children }: ButtonProps) {
+  const cn = primary
+    ? "text-title bg-btnPrimary border-btnPrimary"
+    : "text-content bg-transparent border-content";
+
+  return (
+    <button
+      className={`px-button h-[3rem] border-solid border rounded font-bold ${cn}`}
+    >
+      <div>
+        {text}
+        {children}
+      </div>
+    </button>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    borderRadius: {
+      DEFAULT: ".375rem",
+    },
     extend: {
       backgroundImage: {
         gradient: `linear-gradient(90deg, #F6AD55 0%, #FBD38D 14.58%, #9CE6B3 28.13%, #81E6D9 42.19%, #9DECF9 55.21%, #D6BCFA 68.75%, #FED7D7 83.33%, #FEB2B2 97.40%)`,
@@ -13,6 +16,9 @@ export default {
         bgCard: "rgba(0, 0, 0, 0.24)",
         border: "rgba(255, 255, 255, 0.36)",
         btnPrimary: "rgba(0, 0, 0, 0.48)",
+      },
+      padding: {
+        button: "1.5rem",
       },
       keyframes: {
         scrollUp: {
