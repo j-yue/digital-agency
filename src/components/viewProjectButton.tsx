158081ca5e5f4cab8180d9283996d1b11558c44c
Jeanette Yue <j-yue>
2023-07-15 14:24:38 -0700

    fix - semantically should be link not button

--- src/components/viewProjectButton.tsx
+++ src/components/viewProjectButton.tsx
@@ -4,13 +4,16 @@ import DoubleArrowRightIcon from "./Icons/doubleArrowRightIcon";
 
 export default function ViewProjectButton() {
   return (
-    <button className="flex flex-nowrap flex-row justify-center items-center gap-[1ch] h-[3rem]">
+    <a
+      href="/"
+      className="flex flex-nowrap flex-row justify-center items-center gap-[1ch] h-[3rem]"
+    >
       <Blinking direction="left">View Project</Blinking>
       <Blinking direction="right" reverse={true}>
         <Icon>
           <DoubleArrowRightIcon />
         </Icon>
       </Blinking>
-    </button>
+    </a>
   );
 }
