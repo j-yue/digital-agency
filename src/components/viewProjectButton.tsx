6ab125c2aacb73adcc7e658787409a1988a3e437
Jeanette Yue <j-yue>
2023-07-15 14:10:15 -0700

    Create component

---
+++ src/components/viewProjectButton.tsx
@@ -0,0 +1,16 @@
+import Blinking from "./blinking";
+import Icon from "./icon";
+import DoubleArrowRightIcon from "./Icons/doubleArrowRightIcon";
+
+export default function ViewProjectButton() {
+  return (
+    <button className="flex flex-nowrap flex-row justify-center items-center gap-[1ch] h-[3rem]">
+      <Blinking direction="left">View Project</Blinking>
+      <Blinking direction="right" reverse={true}>
+        <Icon>
+          <DoubleArrowRightIcon />
+        </Icon>
+      </Blinking>
+    </button>
+  );
+}
