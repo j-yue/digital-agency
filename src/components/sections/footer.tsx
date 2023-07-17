c6e1c3c3636c4613c003ad901428b0639cca4052
Jeanette Yue <j-yue>
2023-07-17 15:51:37 -0700

    Remove unused code

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -29,29 +29,3 @@ export default function Footer() {
     </footer>
   );
 }
-
-// export default function Footer() {
-//   // icons
-//   // email
-//   // copyright
-//   return (
-//     <footer className="text-[.875rem] leading-[1.25rem] flex flex-col flex-nowrap justify-center items-center gap-[2rem] pt-[1.75rem] pb-[.75rem]">
-//       <div className="text-title flex flex-row flex-nowrap gap-[2.25rem]">
-//         {[YoutubeIcon, InstagramIcon, FacebookIcon].map((icon, index) => {
-//           return (
-//             <a key={index}>
-//               <Icon>{icon()}</Icon>
-//             </a>
-//           );
-//         })}
-//       </div>
-//       <div className="">pixelate.agency@gmail.com</div>
-//       <div className="flex flex-row flex-nowrap">
-//         <Icon>
-//           <CopyrightIcon />
-//         </Icon>
-//         <span className="ml-[1rem]">All rights reserved by Pixelate.</span>
-//       </div>
-//     </footer>
-//   );
-// }
