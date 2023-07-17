a593498c9a5544be7be95627e4a053cd3ed7e87e
Jeanette Yue <j-yue>
2023-07-16 19:06:09 -0700

    Apply website structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import Nav from "./components/nav";
+import Home from "./components/sections/home";
+import About from "./components/sections/about";
+import Services from "./components/sections/services";
+import Works from "./components/sections/works";
+import Footer from "./components/sections/footer";
+
 function App() {
-  return <main className="font-inter bg-gray-300">digital agency</main>;
+  return (
+    <>
+      <header>
+        <Nav
+          linkArr={[
+            ["Contact Us", "#home"],
+            ["About Us", "#about"],
+            ["Works", "#works"],
+            ["Services", "#services"],
+          ]}
+        />
+      </header>
+
+      <main>
+        <h1 className="sr-only">Pixelate Agency Website</h1>
+        <Home />
+        <About />
+        <Services />
+        <Works />
+      </main>
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;
